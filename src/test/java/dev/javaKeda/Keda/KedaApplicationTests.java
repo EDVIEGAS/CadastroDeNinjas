1a68feac5a04f947920b7471011dbe54eb7a88f0
edson.viegas <edson.viegas@porlande.com>
2025-05-19 16:35:16 +0100

    configuracoes

---
+++ src/test/java/dev/javaKeda/Keda/KedaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javaKeda.Keda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
